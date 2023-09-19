7e72f350583ac8158e2657e5a3a9b62ff682452f
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-19 16:57:08 +0530

    add

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -1,12 +1,14 @@
 import { Box, Button, Flex, HStack, Heading ,Select,Text,Textarea } from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useState } from 'react'
+import Loader from './Loader'
 
 const Convert = () => {
 
   const [language,setLanguage] = useState("")
   const [code,setCode] = useState("")
   const [result,setResult] = useState("")
+  const [loading,setLoading] = useState(false)
   
 
   const handleConvert = () => {
@@ -14,10 +16,13 @@ const Convert = () => {
       code ,
       language
     }
+    setLoading(true)
       axios.post("https://convertbackend.onrender.com/code/convert",codeDetails).then((res)=>{
         // console.log(res.data)
+        setLoading(false)
         setResult(res.data)
       }).catch(err=>{
+        setLoading(false)
         console.log(err)
       })
   }
@@ -26,10 +31,13 @@ const Convert = () => {
     const codeDetails = {
       code 
     }
+    setLoading(true)
       axios.post("https://convertbackend.onrender.com/code/debug",codeDetails).then((res)=>{
         // console.log(res.data)
+        setLoading(false)
         setResult(res.data)
       }).catch(err=>{
+        setLoading(false)
         console.log(err)
       })
   }
@@ -38,56 +46,89 @@ const Convert = () => {
     const codeDetails = {
       code 
     }
+    setLoading(true)
       axios.post("https://convertbackend.onrender.com/code/qualitycheck",codeDetails).then((res)=>{
         // console.log(res.data)
+        setLoading(false)
         setResult(res.data)
       }).catch(err=>{
+        setLoading(false) 
         console.log(err)
       })
   }
 
   return (
-    <Box bg="#0D1117"   h="100vh">
-      <Heading textTransform={"uppercase"} color={"white"}>Code Converter</Heading>
-      <Flex w="90%" m="30px auto"  justifyContent={"space-between"}>
-        <Box w="50%" >   
-          <Flex  alignItems={"center"}>    
-          <Text fontSize={"20px"} color={"white"}>Convert to :-</Text>
-          <Select   value={language} onChange={(e)=>setLanguage(e.target.value)} ml="10px" w="50%">
-            <option  value={"javascript"}>JavaScript</option>
-            <option value={"typescript"}>TypeScript</option>
-            <option value={"java"}>Java</option>
-            <option value={"python"}>Python</option>
-            <option value={"ruby"}>Ruby</option>
-            <option value={"c++"}>C++</option>
-            <option value={"c#"}>C#</option>
-            <option value={"go"}>Go</option>
-          </Select>
-          </Flex>
-        </Box>
-        <Box w="50%">
-          <Flex justifyContent={"center"} alignItems={"center"}>
-            <Button bg="#fb923c" onClick={handleConvert}  borderRadius={"5px 5px 0 0"} color={"white"}>Convert</Button>
-            <Button bg="#fb923c" onClick={handleDebug} borderRadius={"5px 5px 0 0"} m="0 50px" color={"white"}>Debug</Button>
-            <Button bg="#fb923c" onClick={handleQuality} borderRadius={"5px 5px 0 0"} color={"white"}>Quality Check</Button>
-          </Flex>
-        </Box>
-      </Flex>
+    <>
+      <Box bg="#0D1117"  p="10px">
+         <Heading textTransform={"uppercase"} color={"white"} textAlign={"center"}>Code Converter</Heading>
+         <Flex w="90%" m="30px auto" direction={{base:"column",sm:"column",md:"column",lg:"row",xl:"row","2xl":"row"}} justifyContent={{base:"center",sm:"center",md:"center",lg:"space-between",xl:"space-between","2xl":"space-between"}}  >
+           <Box w={{ base: "100%", sm: "100%", md: "50%", lg: "50%", xl: "50%", "2xl": "50%" }} >   
 
-      <Flex w="90%" m="30px auto"  justifyContent={"space-between"}>
+             <Flex direction={{base:"column",sm:"column",md:"column",lg:"row",xl:"row","2xl":"row"}}  alignItems={"center"}>    
 
-  <Box w="48%" h="75vh" bg="#F5F5F5" borderRadius={"3px"}  >   
-  <Textarea border={"1px solid gray"}  placeholder='Write your code here...' value={code} onChange={(e)=>setCode(e.target.value)} w="95%" m="20px auto" h="70vh" />
-  </Box>
+             <Text fontSize={"20px"} color={"white"}>Convert to :-</Text>
+             <Select   value={language} onChange={(e)=>setLanguage(e.target.value)} ml="10px" w="50%">
+               <option  value={"javascript"}>JavaScript</option>
+               <option value={"typescript"}>TypeScript</option>
+               <option value={"java"}>Java</option>
+               <option value={"python"}>Python</option>
+               <option value={"ruby"}>Ruby</option>
+               <option value={"c++"}>C++</option>
+               <option value={"c#"}>C#</option>
+               <option value={"go"}>Go</option>
+             </Select>
+             </Flex>
+           </Box>
+           <Box  w={{ base: "100%", sm: "100%", md: "50%", lg: "50%", xl: "50%", "2xl": "50%" }} >
+             <Flex mt={{base:"10px",sm:"10px",md:"10px",lg:"0px",xl:"0px","2xl":"0px"}} direction={{base:"column",sm:"column",md:"column",lg:"row",xl:"row","2xl":"row"}} justifyContent={"center"} alignItems={"center"}>
+              
+               <Button isDisabled={!code ||  !language} bg="#fb923c" onClick={handleConvert}  borderRadius={"5px 5px 0 0"} color={"white"}>Convert</Button>
 
-  <Box w="48%" h="75vh" bg="#E6E6E6" borderRadius={"3px"}>
-  <Textarea isReadOnly={true} border={"1px solid gray"} placeholder='Response will be here...' value={result} w="95%" m="20px auto" h="70vh" />
+               <Button  isDisabled={!code} bg="#fb923c" onClick={handleDebug} borderRadius={"5px 5px 0 0"} m={{ base: "10px 0", sm: "10px 0", md: "10px 0", lg: "0 50px", xl: "0 50px", "2xl": "0 50px" }} color={"white"}>Debug</Button>
+
+               <Button isDisabled={!code} bg="#fb923c" onClick={handleQuality} borderRadius={"5px 5px 0 0"} color={"white"}>Quality Check</Button>
+             </Flex>
+           </Box>
+         </Flex>
    
-</Box>
-</Flex>
 
+
+
+
+
+
+
+
+
+         <Flex w="90%" m="30px auto" direction={{base:"column",sm:"column",md:"column",lg:"row",xl:"row","2xl":"row"}}   justifyContent={"space-between"}>
+   
+     <Box w={{ base: "100%", sm: "100%", md: "48%", lg: "48%", xl: "48%", "2xl": "48%" }} h="75vh" bg="#F5F5F5" borderRadius={"3px"}  >   
+     <Textarea border={"1px solid gray"}  placeholder='Write your code here...' value={code} onChange={(e)=>setCode(e.target.value)} w="95%" m="10px 10px" h="70vh" />
+     </Box>
+   
+     <Box w={{ base: "100%", sm: "100%", md: "48%", lg: "48%", xl: "48%", "2xl": "48%" }} mt={{base:"10px",sm:"10px",md:"10px",lg:"0px",xl:"0px","2xl":"0px"}} h="75vh" bg="#E6E6E6" borderRadius={"3px"}   display="flex"
+            justifyContent="center"
+            alignItems="center"
+            position="relative">
+     {
+      
+         loading ?   <div className="loader" style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+                <div className="loader-inner">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div> :      <Textarea isReadOnly={true} border={"1px solid gray"} placeholder='Response will be here...' value={result} w="95%" m="10px 10px" h="70vh" />
+
+     }
+      
+   </Box>
+   </Flex>
+   
+        
+       </Box>
      
-    </Box>
+     </>
   )
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+const Loader = ({ size }) => {
+    return (
+      <svg
+        className="loader"
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="12" cy="12" r="10" />
+      </svg>
+    );
+  };
+
+  export default Loader;
